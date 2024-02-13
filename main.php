443cbbb72446ca5062bf709f2995d2d92f47ce62
hojinchang <72773536+hojinchang@users.noreply.github.com>
2024-02-13 15:38:08 -0800

    Display every student in a table in main page

--- main.php
+++ main.php
@@ -2,6 +2,7 @@
 <?php
     // Import globals
     require_once("./globals/globals.php");
+    require_once("./dbinfo.php");
 
     // Import Head component
     require_once("./components/head.php");
@@ -19,9 +20,51 @@
     <main>
         <section class="scriptDemo">
             <h2>Administering DB From a Form</h2>
+            <?php
+                echo "<p>Hello <b>". ucfirst($_SESSION["username"]) ."</b>. You are authorized to view the content</p>";
+            ?>
+
             <h3>Students:</h3>
 
             <a class="add-student" href="./addStudentPage.php">Add a Student</a>
+
+            <?php 
+                // Connect to database
+                $database = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+                if (mysqli_connect_errno() != 0) {
+                    $_SESSION["errorMessages"] = ["Could not connect to the database"];
+                    header("Location: ../index.php");
+                    exit();
+                }
+
+                // Query select every student
+                $query = "SELECT id, firstname, lastname FROM students";
+                $results = $database->query($query);
+
+                if ($results->num_rows > 0) {
+                    // Get table fields
+                    $tableFields = $results->fetch_fields();
+
+                    // Display the students in a table
+                    echo "<table>";
+                    echo "<tr>";
+                    foreach($tableFields as $field) {
+                        echo "<th>$field->name</th>";
+                    }
+                    echo "</tr>";
+
+                    while($record = $results->fetch_row()) {
+                        echo "<tr>";
+                        foreach($record as $data) {
+                            echo "<td>$data</td>";
+                        }
+                        echo "</tr>";
+                    }
+
+                    echo "</table>";
+                }
+
+            ?>
         </section>
     </main>
 
