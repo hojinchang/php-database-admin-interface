c4084db820e721a9d9eeb6ddc7a639ce1dbd9eee
hojinchang <72773536+hojinchang@users.noreply.github.com>
2024-02-13 17:56:22 -0800

    Fix bug where session time resets

--- utilities/sessionGuard.php
+++ utilities/sessionGuard.php
@@ -10,10 +10,8 @@
             $_SESSION["errorMessages"] = ["You have been logged out due to inactivity"];
             header("Location: logout.php");
             exit();
-        } else {
-            // Reset last active
-            $_SESSION['activeTime'] = time();
         }
+        
     } else {
         $_SESSION["errorMessages"] = ["Please log in to view content"];
         header("Location: index.php");
